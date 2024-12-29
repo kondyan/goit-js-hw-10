eb90aa54939878bbb569aa31c9e8f8c7d030af42
kondyan <arsenij.kondratenko@gmail.com>
2024-12-30 00:27:03 +0100

    /

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // Configuration options go here
+  base: "/goit-js-hw-10/", // Base public path when deploying
+  plugins: [], // Add any Vite plugins here
+});
